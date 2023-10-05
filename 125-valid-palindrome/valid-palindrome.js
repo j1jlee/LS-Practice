a9f07621f7541560b461a65a185bfc40b37c9cef
j1jlee <42744694+j1jlee@users.noreply.github.com>
2023-10-05 12:56:45 -0400

    Time: 56 ms (86.44%) | Memory: 44.6 MB (70.38%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  const formattedString = s.replace(/[^a-z0-9]/gi, '').toLowerCase().split('');
+
+  //console.log(formattedString)
+
+  let start = 0;
+  let end = formattedString.length - 1;
+
+  while (start < end) {
+    if (formattedString[start] != formattedString[end]) {
+      return false;
+    }
+
+    start++;
+    end--;
+  }
+
+  return true;
+  
+  //pointer at start, pointer at end
+  //  
+};
+
+//var a = 'Test123*** TEST';
+// var b = a.replace(/[^a-z0-9]/gi, '');
+// console.log(b);
