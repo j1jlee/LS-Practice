1d3eeef7ace3670d45f76ed8887553f6a41179e6
j1jlee <42744694+j1jlee@users.noreply.github.com>
2023-10-05 12:58:48 -0400

    Time: 58 ms (79.91%) | Memory: 44.7 MB (70.38%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -4,11 +4,6 @@
  */
 var isPalindrome = function(s) {
   const formattedString = s.replace(/[^a-z0-9]/gi, '').toLowerCase().split('');
-  //^ everything not within brackets
-  // gi, global, so all catches even after first find
-  // case insensitive
-
-  //console.log(formattedString)
 
   let start = 0;
   let end = formattedString.length - 1;
@@ -17,17 +12,10 @@ var isPalindrome = function(s) {
     if (formattedString[start] != formattedString[end]) {
       return false;
     }
-
     start++;
     end--;
   }
 
   return true;
-  
-  //pointer at start, pointer at end
-  //  
 };
 
-//var a = 'Test123*** TEST';
-// var b = a.replace(/[^a-z0-9]/gi, '');
-// console.log(b);
