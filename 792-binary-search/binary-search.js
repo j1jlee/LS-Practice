693ae4c679a7ab1c226636ce5c95215ec6f52429
j1jlee <42744694+j1jlee@users.noreply.github.com>
2023-10-05 13:51:46 -0400

    Time: 53 ms (69.80%) | Memory: 45.5 MB (10.06%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,5 +4,31 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.indexOf(target)
+    //return nums.indexOf(target)
+
+    //binary search algorithm;
+    //left pointer, right pointer
+    //0, len - 1
+
+    //midpoint is left + right / 2
+    //if mid is target, return mid
+    //if nums[mid] < target
+        //left = nums + 1
+    //else if nums[mid] > target
+        //right = nums - 1
+
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+    
+        const midValue = nums[mid];
+
+        if (target == midValue) return mid;
+        else if (target > midValue) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    return -1;
 };
